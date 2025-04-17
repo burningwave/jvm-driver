47effc06a92eb57684284401d778652ea9a059d3
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-04-17 10:55:05 +0200

    Fixed some code not compatible with Java 7. Releasing new version

--- java/src/test/java/org/burningwave/jvm/test/NativeExecutorTest.java
+++ java/src/test/java/org/burningwave/jvm/test/NativeExecutorTest.java
@@ -71,7 +71,7 @@ public class NativeExecutorTest {
     @Test
     public void test() {
         final NativeExecutor nativeExecutor = NativeExecutor.getInstance();
-        Class<?> cls = TestBean.class;
+        final Class<?> cls = TestBean.class;
         TestBean bean = (TestBean) nativeExecutor.allocateInstance(cls);
         printBean(bean);
 
